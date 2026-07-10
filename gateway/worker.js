316f3ed1103f3cc8e0075c15697cd1f7b622ebc5
MohamedAfzal0719 <afzalmohamed098@gmail.com>
2026-07-10 10:56:40 +0530

    feat: transcode HEVC to H264 for WebRTC compatibility

--- gateway/worker.js
+++ gateway/worker.js
@@ -371,7 +371,7 @@ class CameraBridge {
       '-probesize', '32',
       '-f', inputFormat,
       '-i', 'pipe:0',
-      '-c:v', 'copy',
+      ...(isH265 ? ['-c:v', 'libx264', '-preset', 'ultrafast', '-tune', 'zerolatency', '-crf', '28'] : ['-c:v', 'copy']),
       '-max_muxing_queue_size', '1024',
       '-f', 'rtsp',
       '-rtsp_transport', 'tcp',
